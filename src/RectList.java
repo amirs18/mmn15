38c788cec241ba26d60352fa88462c525dd00548
Amir <amir.shafat1@gmail.com>
2022-01-23 00:19:14 +0200

    all done

--- src/RectList.java
+++ src/RectList.java
@@ -1,19 +1,18 @@
 public class RectList {
+    // variable creation
     private  RectNode _head ;
 
+    /**
+     * first constructor empty constructor creates an empty  RectList
+     */
     public RectList() {
         this._head=null;
     }
 
-   /* public RectList(RectNode _head) {
-        this._head = _head;
-    }
-
-    */
-
     /**
-     *
-     * @param r
+     *addRect is a function that adds a node to the RectList with the values of a given rectangle unless the given rectangle already exist in the RectList
+     * space complexity O(1), running complexity O(n)
+     * @param r the given rectangle
      */
     public void addRect(RectangleA r){
         RectNode _temp_h=_head;
@@ -32,9 +31,10 @@ public class RectList {
     }
 
     /**
-     *
-     * @param p
-     * @return
+     * howManyWithPoint is a function that returns the number of rectangles that have the same SW point like the one given
+     * space complexity O(1), running complexity O(n)
+     * @param p the given SW Point
+     * @return the number of rectangles with the given SW point
      */
     public int howManyWithPoint(Point p){
         RectNode _temp_h=_head;
@@ -46,6 +46,12 @@ public class RectList {
         }
         return counter;
     }
+
+    /**
+     * longestDiagonal is a function that returns the length of the longest diagonal in all the rectangles in the list
+     * space complexity O(1), running complexity O(n)
+     * @return the length of the longest diagonal in all the rectangles in the list
+     */
     public double longestDiagonal(){
         if (_head==null)
             return 0;
@@ -57,6 +63,12 @@ public class RectList {
         }
         return length;
     }
+
+    /**
+     * mostLeftRect is a function that returns the most left SW Point in all the rectangles in the list
+     * space complexity O(1), running complexity O(n)
+     * @return the most left SW Point in all the rectangles in the list
+     */
     public Point mostLeftRect(){
         if (_head==null)
             return null;
@@ -69,6 +81,12 @@ public class RectList {
         }
         return new Point(_temp_p);
     }
+
+    /**
+     * mostLeftRect is a function that returns the highest NE Point in all the rectangles in the list
+     * space complexity O(1), running complexity O(n)
+     * @return the highest NE Point in all the rectangles in the list
+     */
     public Point highestRect(){
         if (_head==null)
             return null;
@@ -81,36 +99,47 @@ public class RectList {
         }
         return new Point(_temp_p);
     }
+
+    /**
+     * minimalContainer is a function that creates and returns the minimal rectangle that can contain all the rectangles in the list
+     * space complexity O(1), running complexity O(n)
+     * @return the minimal rectangle that can contain all the rectangles in the list
+     */
     public RectangleA minimalContainer(){
         if (_head==null)
             return null;
-        return new RectangleA(new Point(mostLeftRect().getX(),lowestRect().getY()),new Point(mostRightRect().getX(),highestRect().getY()));
-    }
-    private Point mostRightRect(){
-        if (_head==null)
-            return null;
-        Point _temp_p=_head.getRect().getPointNE();
+        Point _temp_right=_head.getRect().getPointNE();
+        Point _temp_high=_head.getRect().getPointNE();
+        Point _temp_left=_head.getRect().getPointSW();
+        Point _temp_low=_head.getRect().getPointSW();
+
         RectNode _temp_h=_head;
         while (_temp_h!=null){
-            if (_temp_h.getRect().getPointNE().isRight(_temp_p))
-                _temp_p= _temp_h.getRect().getPointNE();
+            //gets the most right Point in the list
+            if (_temp_h.getRect().getPointNE().isRight(_temp_right))
+                _temp_right= _temp_h.getRect().getPointNE();
+            //gets the highest Point in the list
+            if (_temp_h.getRect().getPointNE().isAbove(_temp_high))
+                _temp_high= _temp_h.getRect().getPointNE();
+            //gets the most left Point in the list
+            if (_temp_h.getRect().getPointSW().isLeft(_temp_left))
+                _temp_left= _temp_h.getRect().getPointSW();
+            //gets the highest Point in the list
+            if (_temp_h.getRect().getPointSW().isUnder(_temp_low))
+                _temp_low= _temp_h.getRect().getPointSW();
             _temp_h=_temp_h.getNext();
         }
-        return new Point(_temp_p);
-    }
-    private Point lowestRect(){
-        if (_head==null)
-            return null;
-        Point _temp_p=_head.getRect().getPointSW();
-        RectNode _temp_h=_head;
-        while (_temp_h!=null){
-            if (_temp_h.getRect().getPointSW().isUnder(_temp_p))
-                _temp_p= _temp_h.getRect().getPointSW();
-            _temp_h=_temp_h.getNext();
-        }
-        return new Point(_temp_p);
+        // returns a new rectangle with the x values of the left and right points and Y values of the high and low points
+         return new RectangleA(new Point(_temp_left.getX(),_temp_low.getY()),new Point(_temp_right.getX(),_temp_high.getY()));
+
     }
 
+    /**
+     * toString is a function that returns a string form of the list
+     * in toString we use the help function nodeCounter in order to count how many nodes are in the list
+     * space complexity O(n), because the string keeps getting bigger in relation to the list, running complexity O(n)
+     * @return a string form of the list
+     */
     @Override
     public String toString() {
         String s="";
@@ -124,6 +153,12 @@ public class RectList {
         }
         return s;
     }
+
+    /**
+     * nodeCounter is a help function for toString that counts the number of nodes in the list
+     * space complexity O(1), running complexity O(n)
+     * @return the number of nodes in the list
+     */
     private int nodeCounter(){
         int counter=0;
         if (_head==null)
--- src/RectNode.java
+++ src/RectNode.java
@@ -2,16 +2,29 @@ public class RectNode {
      private RectangleA _rect;
      private  RectNode _next;
 
+     /**
+      * first constructor, creates a RectNode with a given rectangle and no next RectNode value
+      * @param r the given rectangle
+      */
      public RectNode(RectangleA r) {
           this._rect = new RectangleA(r);
           _next=null;
      }
 
+     /**
+      * second constructor, creates a RectNode with a given rectangle and a given next RectNode value
+      * @param r the given rectangle
+      * @param n the given RectNode
+      */
      public RectNode(RectangleA r, RectNode n) {
           this._rect = new RectangleA(r);
           this._next = n;
      }
 
+     /**
+      * third constructor, copy constructor creates a RectNode with a given RectNode value
+      * @param r the given RectNode value
+      */
      public RectNode (RectNode r){
           if (r!=null) {
                this._rect = new RectangleA(r._rect);
@@ -19,46 +32,43 @@ public class RectNode {
           }
      }
 
+     /**
+      * the function getRect returns the RectangleA value of the RectNode
+      * space complexity O(1), running complexity O(1)
+      * @return the RectangleA value of the RectNode
+      */
      public RectangleA getRect() {
           if (this!=null)
           return new RectangleA(this._rect);
           return null;
      }
+
+     /**
+      * the function getNext returns the Next RectNode value of RectNode
+      * space complexity O(1), running complexity O(1)
+      * @return the Next RectNode value of RectNode
+      */
      public RectNode getNext() {
           if (this!=null)
           return _next;
           return null;
      }
 
+     /**
+      * the function setRect sets the value of the RectangleA to a given RectangleA
+      * space complexity O(1), running complexity O(1)
+      * @param r the given RectangleA
+      */
      public void setRect(RectangleA r) {
           this._rect = new RectangleA(r);
      }
 
+     /**
+      *  the function setNext sets the value of the next RectNode to a given RectNode
+      *  space complexity O(1), running complexity O(1)
+      * @param _next the given setNext
+      */
      public void setNext(RectNode _next) {
           this._next = _next;
      }
-
-     /* public RectNode (RectangleA r){
-         _rect=new RectangleA(r);
-         _next=null;
-    }
-    public RectNode (RectangleA r, RectNode n){
-          _rect= new RectangleA(r);
-          _next=new RectNode(n);
-    }
-    public RectNode (RectNode r){
-          _rect=new RectangleA(r._rect);
-          _next=new RectNode(r._next);
-    }
-    public RectangleA get_rect(){
-          return this._rect;
-    }
-     public RectNode get_next(){
-          return this._next;
-     }
-     public void set_rect(RectangleA r){
-          this._rect=new RectangleA(r);
-     }
-
-     */
 }
